f84a69f85f5a86ef41ea918ef288756ae07032c0
Voropaev Daniil <vorop2004@mail.ru>
2025-12-17 20:54:52 +0300

    first commit

---
+++ src/main/java/com/example/meetnow/MeetNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meetnow/MeetNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
