e51b88d972ef1d0133a481bd067b2a62a620d56b
Voropaev Daniil <vorop2004@mail.ru>
2025-12-18 23:59:13 +0300

    draft ThematicFactorCalculatorStrategy and Interest

---
+++ src/main/java/com/example/meetnow/service/event/calculator/ThematicFactorCalculatorStrategy.java
@@ -0,0 +1,13 @@
+package com.example.meetnow.service.event.calculator;
+
+import com.example.meetnow.service.model.PreviewEvent;
+import com.example.meetnow.service.model.User;
+
+public class ThematicFactorCalculatorStrategy implements FactorCalculatorStrategy {
+    @Override
+    public Double calculate(User user, PreviewEvent event) {
+        User.g
+
+        return 0.0;
+    }
+}
---
+++ src/main/java/com/example/meetnow/service/model/Interest.java
@@ -0,0 +1,21 @@
+package com.example.meetnow.service.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Interest {
+    TENNIS(1, 10, "Tennis"),
+    FOOTBALL(2, 10, "Football"),
+    PAINTING(3, 20, "Painting");
+
+    private final int id;
+    private final int categoryId;
+    private final String name;
+
+    Interest(int id, int categoryId, String name) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+}
