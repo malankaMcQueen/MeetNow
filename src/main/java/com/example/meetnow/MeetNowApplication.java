bb4933eaa29d27bc4f965660b633360e0a2a3907
Daniil <da.voropaev@kpedev.by>
2025-12-27 14:08:15 +0300

    test push

--- src/main/java/com/example/meetnow/MeetNowApplication.java
+++ src/main/java/com/example/meetnow/MeetNowApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class MeetNowApplication {
-
+// tmp
     public static void main(String[] args) {
         SpringApplication.run(MeetNowApplication.class, args);
     }
