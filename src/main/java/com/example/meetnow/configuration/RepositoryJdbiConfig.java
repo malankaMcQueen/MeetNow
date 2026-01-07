464a9fec70bc89008664a5642c28d5f4d393bf9b
Daniil <da.voropaev@kpedev.by>
2026-01-07 14:28:43 +0300

    DELETE ALL JDBI REPOSITORY

--- src/main/java/com/example/meetnow/configuration/RepositoryJdbiConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.meetnow.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RepositoryJdbiConfig {
-}
--- src/main/java/com/example/meetnow/repository/mapper/EventInterestRow.java
+++
@@ -1,8 +0,0 @@
-package com.example.meetnow.repository.mapper;
-
-import com.example.meetnow.service.model.Interest;
-import org.jdbi.v3.core.mapper.Nested;
-import org.jdbi.v3.core.mapper.reflect.ColumnName;
-
-public record EventInterestRow(@ColumnName("event_id") Long eventId, @Nested("interest") Interest interest) {
-}
